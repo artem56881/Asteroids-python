cdcedab8fdbde3c870fd11181e7eca24d4953b8e
q56881 <artem56881@gmail.com>
2025-03-18 13:57:21 +0500

    вынес отрисовку pygame в функции(частично), добавил астероиды, коллизии, рандомную генерацию астероидов.

--- myvenv/game.py
+++ myvenv/game.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import math
 import os
+import random
 
 pygame.init()
 ScreenSize = [800, 600]
@@ -22,26 +23,55 @@ playerAngleVelocity = 5
 
 angleAcceleration = 5
 
+
+timer = 0
+
 velX = 0
 velY = 0
 acceleration = 0.1
 friction = 0.99
 
 
+class Asteroid:
+    x_coordinate = 0
+    y_coordinate = 0
+    type = 0
+    angle = 0
+    speed = 0
+
+    def __init__(self, x, y, type, angle):
+        self.x_coordinate = x
+        self.y_coordinate = y
+        self.type = type
+        self.angle = angle
+
+
+    def fly(self):
+        dir_vector = angle_to_cords(self.angle)
+        self.x_coordinate += dir_vector[0]
+        self.y_coordinate += dir_vector[1]
+
+
+
 def angle_to_cords(angle: int):
     rad = math.radians(angle)
     return math.cos(rad), math.sin(rad)
 
+asteroid1 = Asteroid(random.randint(100, 500), random.randint(100, 500), random.randint(2, 10), random.randint(0, 360))
+asteroid2 = Asteroid(random.randint(100, 500), random.randint(100, 500), random.randint(2, 10), random.randint(0, 360))
+asteroid3 = Asteroid(random.randint(100, 500), random.randint(100, 500), random.randint(2, 10), random.randint(0, 360))
+
+asteroids = [asteroid1, asteroid2, asteroid3]
 
 def update_console(vector):
-    shipSize = 0
+    ship_size: int = 0
     os.system('cls' if os.name == 'nt' else 'clear')
     for i in range(0, ScreenSize[1], screenScale):
         for j in range(0, ScreenSize[0], screenScale):
-            if 1 <= i - int(playerY) <= screenScale + shipSize and 1 <= j - int(playerX) <= screenScale + shipSize:
+            if 1 <= i - int(playerY) <= screenScale + ship_size and 1 <= j - int(playerX) <= screenScale + ship_size:
                 sys.stdout.write("O ")
-            elif (1 <= i - int(vector[1]) <= screenScale + shipSize and 1 <= j - int(
-                    vector[0]) <= screenScale + shipSize):
+            elif (1 <= i - int(vector[1]) <= screenScale + ship_size and 1 <= j - int(
+                    vector[0]) <= screenScale + ship_size):
                 sys.stdout.write("+ ")
             else:
                 sys.stdout.write(". ")
@@ -51,6 +81,11 @@ def update_console(vector):
     sys.stdout.flush()
 
 
+def draw_asteroids(asteroids):
+    for a in asteroids:
+        pygame.draw.circle(screen, (100, 110, 100), (a.x_coordinate, a.y_coordinate), radius=a.type*10)
+
+
 def draw_pygame(player_cords, dir_vector, player_angle, speed, debug):
 
     back_angle = (speed * 12) + 100
@@ -58,14 +93,14 @@ def draw_pygame(player_cords, dir_vector, player_angle, speed, debug):
     ship_width = 15
     ship_length = 35
 
-    vecor_l = angle_to_cords(playerAngle - back_angle)
-    vecor_r = angle_to_cords(playerAngle + back_angle)
+    vector_l = angle_to_cords(playerAngle - back_angle)
+    vector_r = angle_to_cords(playerAngle + back_angle)
 
     player_cords = (player_cords[0] - dir_vector[0] * ship_offset, player_cords[1] - dir_vector[1] * ship_offset)
     head_cords = (player_cords[0] + dir_vector[0] * ship_length, player_cords[1] + dir_vector[1] * ship_length)
 
-    left_point_cords = (player_cords[0] + vecor_l[0] * ship_width, player_cords[1] + vecor_l[1] * ship_width)
-    right_point_cords = (player_cords[0] + vecor_r[0] * ship_width, player_cords[1] + vecor_r[1] * ship_width)
+    left_point_cords = (player_cords[0] + vector_l[0] * ship_width, player_cords[1] + vector_l[1] * ship_width)
+    right_point_cords = (player_cords[0] + vector_r[0] * ship_width, player_cords[1] + vector_r[1] * ship_width)
 
     ship_color = (205, 205, 205)
 
@@ -75,10 +110,11 @@ def draw_pygame(player_cords, dir_vector, player_angle, speed, debug):
     pygame.draw.line(screen, ship_color, head_cords, right_point_cords, width=3)
     pygame.draw.line(screen, ship_color, head_cords, left_point_cords, width=3)
 
+
     if debug:
-        text = font.render(f"{player_angle}, {velX:.3f}, {velY:.3f}, bAngle: {back_angle:.3f}", True, (255, 255, 255))
-        screen.blit(text, (10, 10))  # draw
-    pygame.display.flip()
+        text = font.render(f"{player_angle}, {velX:.3f}, {velY:.3f}, bAngle: {back_angle:.3f}",True, (255, 255, 255))
+        screen.blit(text, (10, 10))  # draw text
+    # pygame.display.flip()
 
 
 running = True
@@ -120,9 +156,35 @@ while running:
 
     vector = angle_to_cords(playerAngle)
     player_coords = (playerX, playerY)
+
+    # Обновление астероидов
+    for a in asteroids:
+        a.fly()
+        if a.x_coordinate > ScreenSize[0]:
+            a.x_coordinate = 0
+        if a.x_coordinate < 0:
+            a.x_coordinate = ScreenSize[0]
+
+        if a.y_coordinate > ScreenSize[1]:
+            a.y_coordinate = 0
+        if a.y_coordinate < 0:
+            a.y_coordinate = ScreenSize[1]
+
+
+
+        # Проверка на столкновение с астероидом
+        if (((playerX-a.x_coordinate)**2 + (playerY-a.y_coordinate)**2)**0.5)//10 <= a.type:
+            running = False
+        # if a.type == 10:
+        #     print(((playerX-a.x_coordinate)**2 + (playerY-a.y_coordinate)**2)**0.5, a.type)
+
+    # Отрисовка астероидов
+    draw_asteroids(asteroids) # Должен быть вызван до метода draw_pygame
+    # if timer >= 10:
     draw_pygame(player_coords, vector, playerAngle, abs((velX+velY)/2), False)
-
-
+        # timer = 0
+    # timer += 1
+    pygame.display.flip()
     # Отрисовка в консоли
     # update_console((playerX + vector[0] * 10, playerY + vector[1] * 10))
 
