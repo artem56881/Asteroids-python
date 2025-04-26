8133f3e6a4dc079cdde700fabbbbbe967fa13ba1
q56881 <artem56881@gmail.com>
2025-04-26 14:58:41 +0500

    nc2

--- logic/teammate_logic.py
+++ logic/teammate_logic.py
@@ -17,7 +17,7 @@ def update_teammate(ship: Ship, asteroids, saucers, player):
 
     for obj in asteroids + saucers:
         player_distance = math.hypot(ship.x - player.x, player.y - ship.y)
-        if player_distance < 300: # если расстояние до игрока
+        if player_distance < 300:
             vel_x, vel_y = angle_to_coords(obj.angle, obj.speed)
 
             predicted_x = obj.x + vel_x * prediction_time
@@ -26,7 +26,7 @@ def update_teammate(ship: Ship, asteroids, saucers, player):
             if find_range(ship.x, ship.y, predicted_x, predicted_y) <= max_shooting_range:
                 asteroid_direction = math.atan2(predicted_y - ship.y, predicted_x - ship.x)
                 angle_diff = math.degrees(asteroid_direction - ship_direction)
-                angle_diff = (angle_diff + 360) % 360  # Normalize angle to [0, 360)
+                angle_diff = (angle_diff + 360) % 360
 
                 if angle_diff <= fov / 2 or angle_diff >= 360 - fov / 2:
                     if angle_diff < min_angle_diff:
@@ -66,7 +66,7 @@ def update_teammate(ship: Ship, asteroids, saucers, player):
         target_angle = math.degrees(target_direction)
 
         angle_delta = target_angle - ship.angle
-        angle_delta = (angle_delta + 180) % 360 - 180  # Normalize angle to [-180, 180)
+        angle_delta = (angle_delta + 180) % 360 - 180
         if angle_delta > ship.turn_speed:
             angle_delta = ship.turn_speed
         elif angle_delta < -ship.turn_speed:
@@ -80,7 +80,7 @@ def update_teammate(ship: Ship, asteroids, saucers, player):
         nearest_angle = math.degrees(nearest_direction)
 
         angle_delta = nearest_angle - ship.angle
-        angle_delta = (angle_delta + 180) % 360 - 180  # Normalize angle to [-180, 180)
+        angle_delta = (angle_delta + 180) % 360 - 180
         if angle_delta > ship.turn_speed:
             angle_delta = ship.turn_speed
         elif angle_delta < -ship.turn_speed:
